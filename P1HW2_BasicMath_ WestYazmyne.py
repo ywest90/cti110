0d91e2ceb27a895445956769be2bb2adc78153b5
Yazmyne West <82738310+ywest90@users.noreply.github.com>
2021-04-18 13:40:17 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_ WestYazmyne.py
@@ -0,0 +1,27 @@
+# Using IDLE Math
+# 4/18/20
+# CTI-110 P1HW2 - Basic Math
+# Yazmyne West
+#
+print("")
+print("START")
+print("")
+
+print('Choose Two Numbers For Computing:')
+firstNum=int(input('Enter the first number:'))
+secondNum=int(input('Enter the second number:'))
+print("")
+
+print('Your Numbers:')
+print('First number entered:' , firstNum)
+print('Second number entered:' , secondNum)
+print("")
+
+print('Sum of both numbers:' , firstNum + secondNum)
+print('Result of Mutiplying the two numbers:' , firstNum * secondNum)
+print("")
+
+print('All numbers have been computed.')
+print("")
+print('END & CLOSE PROGRAM')
+
